27abee865045fe51fb9d75888156ae0db6234770
mamta349 <159692229+mamta349@users.noreply.github.com>
2024-10-22 13:56:36 +0000

    conversionOprasion

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accoutId = 234556;
+let accoutPassword = "9876543";
+var accoutCity = "jaipur";
+accoutEmail = "patidarmamta@gmail.com";
+let accoutState ;
+
+console.log(accoutId);
+
+console.table([accoutCity, accoutId, accoutPassword, accoutState, accoutEmail])
---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,7 @@
+   console.log(typeof "vedika");       // string
+   
+   console.log(typeof "undefine");    // undefine
+   console.log(typeof "null");        //  object
+   
+   
+   
