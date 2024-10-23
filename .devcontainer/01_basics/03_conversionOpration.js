35493a31d5a5ee4b685d3cd8ba5ff3e0fcfa9d61
mamta349 <159692229+mamta349@users.noreply.github.com>
2024-10-23 13:25:57 +0530

    conversion

--- .devcontainer/01_basics/03_conversionOpration.js
+++ .devcontainer/01_basics/03_conversionOpration.js
@@ -1,1 +1,43 @@
-console.log("hello")
+
+let score = "66d" ;        // ager 66 per null hota to 0 , true = 1, undefine = nan print hota
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);      // print NaN mean ( not a number)
+console.log(typeof score);
+
+
+// *********************Opration*********************************//
+
+let num = 4;
+let num2 = 6;
+let num3 = num + num2;
+console.log(num3);
+
+
+let value = 9 ;
+let nagvalue = -value;
+console.log(nagvalue);
+
+console.log(2+2);
+console.log(22-3);
+console.log(4*1);
+console.log(5/5);
+
+console.log("2" + 1);            // output is 2 or 1  = 21
+console.log(1 + "2");                       // output is 1 or 2  = 12
+console.log("1" + 2 + 2);          // output is 1 or 2 or 2  = 122
+console.log(1+ 2+ "3");                   // output is 1 or 2 or 3  = 33   1 + 3 = 3 and 3 = 33
+
+
+console.log(true);                   // true
+console.log(+true);                 // 1
+console.log(false);              // false
+console.log(+false);            // output  0
+
+
+
+
+
