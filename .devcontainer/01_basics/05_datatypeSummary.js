6f27926b34dc8d59ffb228f86673abad9a5e9ce7
mamta349 <159692229+mamta349@users.noreply.github.com>
2024-10-23 16:01:16 +0530

    string

---
+++ .devcontainer/01_basics/05_datatypeSummary.js
@@ -0,0 +1,33 @@
+
+     // primitive data type are = string ,  number  , symbol, boolean , null , undedine , BigInt
+const name = "vedika";
+
+let num = 77;
+
+let id = Symbol("2233");
+
+let kuku = true;
+let outline = null;
+
+let  city ;
+
+let number = 77.8;
+
+
+//  non primitive data type is =  array , object, function ;
+
+let array = ["mayu", "vedu", "avii", "kuku"];      
+
+   {
+    name = "vedika";
+    age = 23;                  // semicolumn ke under  object likhe jate hai;
+}
+
+MathMLElement(55)       // this is function;
+
+console.log(typeof array);
+// console.log(typeof Object);
+console.log(typeof function);
+
+
+
